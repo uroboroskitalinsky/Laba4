67786d0c1d04e9959c66f151bda3bf93134be8d2
Роберт Скиталинский <ro@MacBook-Air-Robert.local>
2021-12-13 19:22:05 +0300

    ork

---
+++ labaVVP4/main.cpp
@@ -0,0 +1,84 @@
+//
+//  main.cpp
+//  labaVVP4
+//
+//  Created by Роберт Скиталинский on 07.10.2021.
+//
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double S, P, a, b;
+    cout <<"Print A and B:  " ; cin >> a >> b;
+    S = a * b;
+    P = 2 * (a + b);
+    cout << S << "\n" << P << "\n";
+    return 0;
+}
+/*
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double d, L,c;
+    cout << "Print D:  "; cin >> d;
+    c = 3.14;
+    L = d * c;
+    cout << L << "\n";
+    return 0;
+    
+}
+/*
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double a , b , SrArifm;
+    cout <<"Print A and B:  " << "\n"; cin >> a >> b;
+    SrArifm = (a + b) / 2;
+    cout << SrArifm << "\n";
+    return 0;
+}
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double a, b, Summa, Raznost, Proizv, Chastkv;
+    cout << "Print A and B:  " << "\n" ; cin >> a >> b;
+    Summa = pow(a,2) + pow(b,2);
+    Raznost = pow(a,2) - pow(b,2);
+    Proizv = pow(a,2) * pow(b,2);
+    Chastkv = pow(a,2) / pow(b,2);
+    cout << Summa << "\n" << Raznost << "\n" << Proizv << "\n" << Chastkv << "\n";
+    return 0;
+}
+
+
+
+
+ #include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double a, b, c, d, f, g;
+    cin >> a >> b;
+    c = fabs(a) * fabs(b);
+    d = fabs(a) - fabs(b);
+    f = fabs(a) + fabs(b);
+    g = fabs(a) / fabs(b);
+    cout << c << "\n" << d << "\n" << f << "\n" << g << "\n";
+    return 0;
+}*/
+
+
+
+
+
+
+
